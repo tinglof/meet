56538e242dc6d53accf4597084c7da8a1c4992b7
Johan <joti1454@student.su.se>
2017-02-17 22:04:49 +0100

    tickoff + emptycheck

--- src/list_container.js
+++ src/list_container.js
@@ -6,22 +6,33 @@ class ListContainer extends Component {
     super(props)
     const arr = [];
     this.state = { items: arr };
+
+    this.addItem = this.addItem.bind(this);
+    this.enterPress = this.enterPress.bind(this);
   }
 
    addItem() {
-    const arrayCopy = this.state.items.slice();
 
-    var newItem = {
-      text: this.textInput.value,
-      id: Date.now()
-    };
+    if(this.textInput.value != ""){
+      const arrayCopy = this.state.items.slice();
 
-    arrayCopy.push(newItem);
-    this.textInput.value = "";
+      var newItem = {
+        text: this.textInput.value,
+        id: Date.now()
+      };
 
-    this.setState({
-      items: arrayCopy
-    });
+      arrayCopy.push(newItem);
+      this.textInput.value = "";
+
+      this.setState({
+        items: arrayCopy
+      });
+    }
+  }
+
+  enterPress(e){
+    if(e.key === 'Enter')
+      this.addItem();
   }
 
   render(){
@@ -30,10 +41,10 @@ class ListContainer extends Component {
 
       <div className="col-xs-4 col-xs-offset-4">
       <div className="input-group">
-        <input className="form-control" type="text" ref={ (input) =>  { this.textInput = input; }}
+        <input className="form-control" onKeyPress={ this.enterPress } type="text" ref={ (input) =>  { this.textInput = input; }}
         placeholder="enter task"/>
         <span className="input-group-btn">
-          <button type="button" className="button-default btn" onClick={ () => this.addItem() }>Add</button>
+          <button type="button" className="button-default btn" onClick={ this.addItem }>Add</button>
         </span>
       </div>
         <ListItem items={this.state.items}/>
--- src/list_item.js
+++ src/list_item.js
@@ -2,9 +2,33 @@ import React, { Component } from 'react';
 
 class ListItem extends Component {
 
+  constructor () {
+    super();
+
+    this.state = {
+      checked: true
+    }
+  }
+
+  boxChecked(){
+    console.log("körs");
+
+    this.setState({
+      checked: !this.state.checked
+    })
+  }
 
   render() {
-    return (<li key={this.props.id} className="list-group-item">{this.props.text}</li>)
+    return (
+
+          <div>
+            <li key={this.props.id} className={"list-group-item " + (this.state.checked ? "" : "done" )}>
+              {this.props.text}<input onChange={ () => this.boxChecked() } type="checkbox" className="pull-right" />
+            </li>
+          </div>
+
+
+    )
   }
 
 }
